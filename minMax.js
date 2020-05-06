2c9e95ef06a06525d1ee7bb7f6a505bab39d7a2f
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-05-06 20:37:45 +0200

    sprytny spread

--- minMax.js
+++ minMax.js
@@ -3,24 +3,9 @@
 //Implement a function that returns the minimal and the maximal value of a list (in this order).
 
 function getMinMax(arr) {
-  function sortowanko(a, b) {
-    if (a < b) return -1;
-
-    if (a > b) return 1;
-    return 0;
-  }
-
-  const tab = arr.sort(sortowanko);
-  console.log(tab);
-  const dlugosc = tab.length - 1;
-  const wynik = [];
-
-  wynik.push(tab[0]);
-  wynik.push(tab[dlugosc]);
-
-  console.log(wynik);
+  console.log([Math.min(...arr), Math.max(...arr)]);
 }
 
 //getMinMax([1]); //[1,1];
 //getMinMax([1, 2]); //[1,2];
-getMinMax( [2,1,13,44,22,62,101,213,432,543,213,43,122,3,4,7,9,0] ); //[1,2];
+getMinMax( [2,1,13,44,22,62,101,213,432,543,213,43,122,3,4,7,9,0] );
